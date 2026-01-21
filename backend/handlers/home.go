9b7867282018ec2f046e20f0df2d6cddfb0faa19
abelkhadir <abelkhadir@gmail.com>
2026-01-21 12:34:15 +0100

    initial commit

--- backend/handlers/home.go
+++ backend/handlers/home.go
@@ -1,10 +1,9 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the Home Page!")
+	http.ServeFile(w, r, "./frontend/index.html")
 }
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 
 	"real/backend/handlers"
 )
@@ -10,10 +10,15 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	// backend routes
 	mux.HandleFunc("/", handlers.HomeHandler)
 	mux.HandleFunc("/register", handlers.RegisterHandler)
 	mux.HandleFunc("/login", handlers.LoginHandler)
 
-	fmt.Println("Server has started on http://localhost:8080")
+	// frontend (HTML, CSS, JS)
+	fs := http.FileServer(http.Dir("./frontend/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", mux)
 }
